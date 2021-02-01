399f83d6a8dd2a6c453f83074693efe71c6fc376
loovjo <jonathan.loov@gmail.com>
2021-02-01 11:11:54 +0100

    Display data, simple editing

--- client/handler.js
+++ client/handler.js
@@ -16,7 +16,7 @@ SPACING = 0.015;
 CURRENT_IDX = 0;
 ORIENTATION = 0;
 
-function draw(idx, corners, center) {
+function draw(idx, assoc_data, corners, center) {
     let id = "tile-" + idx;
 
     var is_new;
@@ -45,6 +45,9 @@ function draw(idx, corners, center) {
     inner.innerText = idx;
 
     inner.classList.add("cell-inner");
+    if (Object.keys(assoc_data).length > 0) {
+        inner.classList.add("has-data");
+    }
     if (CURRENT_IDX == idx) {
         outer.classList.add("active");
     } else {
@@ -91,10 +94,8 @@ function draw(idx, corners, center) {
     inner.setAttribute("style", "clip-path: " + clippath);
 
     // What way is the thing facing?
-    // console.log(corners, center);
     let angle_rad = (math.pi * 2 + math.arg(math.sub(math.div(math.add(corners[0], corners[1]), 2), center))) % (2 * math.pi);
     let orientation = angle_rad / (2 * math.pi) * 7;
-    // console.log(angle_c1);
     inner.innerText += "\n" + (0 | 10 * orientation) / 10;
 
     outer.onclick = e => {
@@ -120,7 +121,7 @@ function render(tile, path) {
     let fns = [];
     let [corners, [center]] = transform_poly_along_path(origin_corners, path, [math.complex(0, 0)]);
     corners = reorient(corners, tile.orientation);
-    fns.push(draw(tile.idx, corners, center));
+    fns.push(draw(tile.idx, tile.assoc_data, corners, center));
     for (var i = 0; i < tile.neighbours.length; i++) {
         if (tile.neighbours[i] !== null) {
             let new_path = [...path, i];
@@ -138,6 +139,89 @@ function get_all_ids(tile) {
         }
     }
     return ids;
+}
+
+EDITING_FIELD = null;
+
+function render_cell_data(data) {
+    let cell_data = document.getElementById("cell-data");
+    cell_data.innerHTML = "";
+    if (Object.keys(a.assoc_data).length > 0) {
+
+        let keys = Object.keys(a.assoc_data);
+        keys.sort();
+        for (k of keys) {
+            let v = a.assoc_data[k];
+            let id = encodeURIComponent(k);
+
+            let k_div = document.createElement("div");
+            k_div.classList.add("cell-property");
+            cell_data.appendChild(k_div);
+
+            let label = document.createElement("label");
+            label.element_id = id;
+            var lab = document.createElement("p");
+            lab.classList.add("prop-name");
+            lab.innerText = k;
+            label.appendChild(lab);
+            k_div.appendChild(label);
+
+            if (EDITING_FIELD === k) {
+                let data = document.createElement("textArea");
+                data.id = id;
+                data.innerText = v;
+                label.appendChild(data);
+
+                let auth_lab = document.createElement("label");
+                var lab = document.createElement("p");
+                lab.classList.add("prop-name");
+                lab.innerText = "Author (only for logs)";
+                auth_lab.appendChild(lab);
+                k_div.appendChild(auth_lab);
+
+                let author_text = document.createElement("input");
+                author_text.type = "text";
+                author_text.id = "author";
+                auth_lab.appendChild(author_text);
+                author_text.oninput = () => { document.getElementById("author").classList.remove("danger"); };
+
+                let delete_button = document.createElement("button");
+                delete_button.innerText = "Delete me.";
+                delete_button.classList.add("danger");
+                k_div.appendChild(delete_button)
+
+                let submit_button = document.createElement("button");
+                submit_button.innerText = "Submit";
+                k_div.appendChild(submit_button);
+
+                submit_button.onclick = async () => {
+                    let name = document.getElementById("author").value;
+                    if (name === "") {
+                        document.getElementById("author").classList.add("danger");
+                        return;
+                    }
+                    // TODO: do post
+                    EDITING_FIELD = null;
+                    await run();
+                };
+            }
+            else {
+                let edit_button = document.createElement("button");
+                edit_button.innerText = "Edit!";
+                k_div.appendChild(edit_button)
+
+                edit_button.onclick = () => { EDITING_FIELD = k; render_cell_data(data); };
+
+                let data = document.createElement("p");
+                data.id = id;
+                data.classList.add("prop-data");
+                data.innerText = v;
+                label.appendChild(data);
+            }
+            cell_data.appendChild(document.createElement("hr"));
+        }
+    }
+
 }
 
 async function run() {
@@ -159,5 +243,7 @@ async function run() {
     }
     let fns = render(a, []);
     requestAnimationFrame(() => { for (fn of fns) { fn() } });
+
+    render_cell_data(a.assoc_data);
 }
 run();
